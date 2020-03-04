2584d0659a0113920b64af0bc156baae01cc2af1
Anders Wiggers <anders.bille.wiggers@post.au.dk>
2020-03-04 16:03:08 +0100

    add folders

--- index.js
+++ index.js
@@ -5,34 +5,51 @@ const fs = require('fs');
 let version = require('./version.json');
 
 const fil = [];
-const filler = 'Procedure for overdragelse af et domaenenavn.pdf';
+let filler = 'enter name';
 
 const directoryPath = path.join(__dirname, 'card');
+const directoryPathCardBack = path.join(__dirname, 'cardback');
 
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function(err, files) {
-	//handling error
+fs.readdir(directoryPathCardBack, (err, files) => {
 	if (err) {
 		return console.log('Unable to scan directory: ' + err);
 	}
-	//listing all files using forEach
 	files.forEach(function(file) {
 		// Do whatever you want to do with the file
-		fil.push(`${__dirname}/card/${file}`);
-		fil.push(`${__dirname}/cardback/${filler}`);
+		if (file != '.gitkeep') {
+			filler = file;
+		}
 	});
 
-	version.version = version.version + 1;
-
-	fs.writeFile('version.json', JSON.stringify(version), function(err) {
+	fs.readdir(directoryPath, function(err, files) {
+		//handling error
 		if (err) {
-			console.log(err);
+			return console.log('Unable to scan directory: ' + err);
 		}
-		merge(fil, `finalcards_${version}.pdf`, function(err) {
-			if (err) {
-				return console.log(err);
+		//listing all files using forEach
+		files.forEach(function(file) {
+			// Do whatever you want to do with the file
+			if (file != '.gitkeep') {
+				fil.push(`${__dirname}/card/${file}`);
+				fil.push(`${__dirname}/cardback/${filler}`);
 			}
-			console.log('Successfully merged!');
 		});
+
+		console.log(fil);
+		// version.version = version.version + 1;
+
+		// fs.writeFile('version.json', JSON.stringify(version), function(err) {
+		// 	if (err) {
+		// 		console.log(err);
+		// 	}
+		// 	merge(fil, `finalcards_${version}.pdf`, function(err) {
+		// 		if (err) {
+		// 			return console.log(err);
+		// 		}
+		// 		console.log('Successfully merged!');
+		// 	});
+		// });
 	});
 });
+
+//passsing directoryPath and callback function
